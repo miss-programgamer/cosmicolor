5b7d6218621274c293cc0dde5ae2dc271d4b02b4
Mireille Arseneault <mireille@heartcoded.games>
2025-10-12 00:38:09 -0300

    Checking stdout & stderr individually

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## v0.1.4 - Future
+
+- Now checking STDOUT and STDERR separately. This means piping to a file will no longer disable colors for STDERR & vice-versa.
+- Fixed bugs in `cwritef` that had gone unnoticed due to low usage on my part.
+
 ## v0.1.3 - October 2nd 2025
 
 - Made console automatically use UTF-8 on Windows.
--- source/cosmicolor/package.d
+++ source/cosmicolor/package.d
@@ -7,15 +7,26 @@ import std.traits;
 import cosmicolor.parser;
 
 /** 
- * Whether the environment supports color & the user wants them.
+ * Whether STDOUT supports colors & the user wants them.
  * 
- * On Unix platforms, this is checked using `isatty` and `NO_COLOR`.
+ * On Unix platforms, this is checked using `isatty` and `$NO_COLOR`.
  * 
  * On Windows, the library tries to enable virtual terminal processing to
  *   enable ANSI escape sequences. That is, if the output stream is determined
- *   to be `FILE_TYPE_CHAR` and `NO_COLOR` is not set, of course.
+ *   to be `FILE_TYPE_CHAR` and `$env:NO_COLOR` is not set, of course.
  */
-const bool enable_color;
+const bool enable_color_out;
+
+/** 
+ * Whether STDERR supports colors & the user wants them.
+ * 
+ * On Unix platforms, this is checked using `isatty` and `$NO_COLOR`.
+ * 
+ * On Windows, the library tries to enable virtual terminal processing to
+ *   enable ANSI escape sequences. That is, if the output stream is determined
+ *   to be `FILE_TYPE_CHAR` and `$env:NO_COLOR` is not set, of course.
+ */
+const bool enable_color_err;
 
 /** 
  * Behaves the same as $(D writef) except for colorizing its output.
@@ -25,29 +36,51 @@ const bool enable_color;
  *   args = Items to write.
  */
 void cwritef(alias fmt, A...)(A args) @safe
-	if (isSomeString!(typeof(fmt)))
+	if (isSomeString!(typeof(fmt)) && !is(A[0] : File))
 {
-	if (enable_color)
+	if (enable_color_out)
 	{
 		enum cfmt = parseFmt(true, fmt);
-		args[0].writef!cfmt(args[1 .. $]);
+		writef!cfmt(args);
 	}
 	else
 	{
 		enum cfmt = parseFmt(false, fmt);
-		args[0].writef!cfmt(args[1 .. $]);
+		writef!cfmt(args);
+	}
+}
+
+/** ditto */
+void cwritef(alias fmt, A...)(File file, A args) @safe
+	if (isSomeString!(typeof(fmt)))
+{
+	if (file.enable_color)
+	{
+		enum cfmt = parseFmt(true, fmt);
+		file.writef!cfmt(args);
+	}
+	else
+	{
+		enum cfmt = parseFmt(false, fmt);
+		file.writef!cfmt(args);
 	}
 }
 
 /** ditto */
 void cwritef(Char, A...)(in Char[] fmt, A args) @safe
-	if (isSomeString!(Char[]))
-{ writef(parseFmt(enable_color, fmt), args); }
+	if (isSomeString!(typeof(fmt)) && !is(A[0] : File))
+{
+	auto cfmt = parseFmt(enable_color_out, fmt);
+	writef(cfmt, args);
+}
 
 /** ditto */
 void cwritef(Char, A...)(File file, in Char[] fmt, A args) @safe
-	if (isSomeString!(Char[]))
-{ file.writef(parseFmt(enable_color, fmt), args); }
+	if (isSomeString!(typeof(fmt)))
+{
+	auto cfmt = parseFmt(file.enable_color, fmt);
+	file.writef(cfmt, args);
+}
 
 /** 
  * Behaves the same as $(D writefln) except for colorizing its output.
@@ -57,9 +90,9 @@ void cwritef(Char, A...)(File file, in Char[] fmt, A args) @safe
  *   args = Items to write.
  */
 void cwritefln(alias fmt, A...)(A args) @safe
-	if (isSomeString!(typeof(fmt)))
+	if (isSomeString!(typeof(fmt)) && !is(A[0] : File))
 {
-	if (enable_color)
+	if (enable_color_out)
 	{
 		enum cfmt = parseFmt(true, fmt);
 		writefln!cfmt(args);
@@ -75,27 +108,33 @@ void cwritefln(alias fmt, A...)(A args) @safe
 void cwritefln(alias fmt, A...)(File file, A args) @safe
 	if (isSomeString!(typeof(fmt)))
 {
-	if (enable_color)
+	if (file.enable_color)
 	{
 		enum cfmt = parseFmt(true, fmt);
-		file.writefln!cfmt(args[1 .. $]);
+		file.writefln!cfmt(args);
 	}
 	else
 	{
 		enum cfmt = parseFmt(false, fmt);
-		file.writefln!cfmt(args[1 .. $]);
+		file.writefln!cfmt(args);
 	}
 }
 
 /** ditto */
 void cwritefln(Char, A...)(in Char[] fmt, A args) @safe
-	if (isSomeString!(Char[]))
-{ writefln(parseFmt(enable_color, fmt), args); }
+	if (isSomeString!(typeof(fmt)) && !is(A[0] : File))
+{
+	auto cfmt = parseFmt(enable_color_out, fmt);
+	writefln(cfmt, args);
+}
 
 /** ditto */
 void cwritefln(Char, A...)(File file, in Char[] fmt, A args) @safe
-	if (isSomeString!(Char[]))
-{ file.writefln(parseFmt(enable_color, fmt), args); }
+	if (isSomeString!(typeof(fmt)))
+{
+	auto cfmt = parseFmt(file.enable_color, fmt);
+	file.writefln(cfmt, args);
+}
 
 /** 
  * Colorizes a format string and applies the given items to it.
@@ -108,27 +147,64 @@ void cwritefln(Char, A...)(File file, in Char[] fmt, A args) @safe
  */
 auto cformat(alias fmt, A...)(A args) @safe
 	if (isSomeString!(typeof(fmt)))
-{ return format!(parseFmt(enable_color, fmt))(args); }
+{
+	enum cfmt = parseFmt(enable_color, fmt);
+	return format!cfmt(args);
+}
 
 /** ditto */
 auto cformat(Char, A...)(in Char[] fmt, A args) @safe
-	if (isSomeString!(Char[]))
-{ return format(parseFmt(enable_color, fmt), args); }
+	if (isSomeString!(typeof(fmt)))
+{
+	auto cfmt = parseFmt(enable_color, fmt);
+	return format(cfmt, args);
+}
+
+/** 
+ * Check whether colors should be enabled for a given file.
+ * 
+ * Params:
+ *   file = The file to check.
+ * 
+ * Returns: whether the file should receive colored output.
+ */
+private @property bool enable_color(File file) @trusted
+{
+	version (Windows)
+	{
+		if (file.windowsHandle == GetStdHandle(STD_ERROR_HANDLE))
+		{ return enable_color_err; }
+		else
+		{ return enable_color_out; }
+	}
+	else version (Posix)
+	{
+		import core.sys.posix.stdio;
+		import core.sys.posix.unistd;
+
+		if (file.fileno == STDERR_FILENO)
+		{ return enable_color_err; }
+		else
+		{ return enable_color_out; }
+	}
+}
 
 shared static this() @trusted
 {
 	import std.process : environment;
 	import std.range : empty;
 
-	enable_color = environment.get("NO_COLOR", "").empty;
+	const no_color = !environment.get("NO_COLOR", "").empty;
+
+	enable_color_out = !no_color;
+	enable_color_err = !no_color;
 
 	version (Windows)
 	{
 		import core.sys.windows.windows;
 
-		enable_color = enable_color
-			&& isCharFile(STD_OUTPUT_HANDLE)
-			&& isCharFile(STD_ERROR_HANDLE);
+		enable_color_out &= isCharFile(STD_OUTPUT_HANDLE);
+		enable_color_err &= isCharFile(STD_ERROR_HANDLE);
 
 		if (!isVirtualTerminal(STD_OUTPUT_HANDLE))
 		{ cast(void) setVirtualTerminal(STD_OUTPUT_HANDLE); }
@@ -142,9 +218,8 @@ shared static this() @trusted
 	{
 		import core.sys.posix.unistd;
 
-		enable_color = enable_color
-			&& isatty(STDOUT_FILENO)
-			&& isatty(STDERR_FILENO);
+		enable_color_out &= isatty(STDOUT_FILENO);
+		enable_color_err &= isatty(STDERR_FILENO);
 	}
 }
 
